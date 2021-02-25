d352e2626fc83ce051b41c11a59adf7dc8419078
RyanSu <563607356@qq.com>
2021-02-25 19:09:00 +0800

    change

---
+++ 3-原生AJAX/6-取消请求/server.js
@@ -0,0 +1,26 @@
+// 引入
+const express=require('express')
+// 创建express对象
+const app=express()
+// 创建路由规则
+// request是对请求报文的封装
+// response是对响应报文的封装
+app.get('/test',(request,response)=>{
+  // 设置响应头 允许跨域
+  response.setHeader('Access-Control-Allow-Origin','*')
+  // 设置数据
+  const data={
+    name:'Ryan'
+  }
+  // 对象 ==> 字符串
+  let str=JSON.stringify(data)
+  // 延时3s之后 响应字符串
+  setTimeout(()=>{
+    response.send(str)
+  },3000)
+
+})
+// 监听端口，启动服务
+app.listen(8001,()=>{
+  console.log('服务已启动，8001端口监听中')
+})
